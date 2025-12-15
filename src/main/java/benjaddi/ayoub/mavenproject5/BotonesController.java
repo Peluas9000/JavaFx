7b015a9a6558aed398abfa21e2c7375a82a91102
alumno <ayoubbenjaddi5@gmail.com>
2025-12-15 12:44:23 +0100

    terminado los 4 botones con sus acciones y keyactions

--- src/main/java/benjaddi/ayoub/mavenproject5/BotonesController.java
+++ src/main/java/benjaddi/ayoub/mavenproject5/BotonesController.java
@@ -1,80 +1,62 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package benjaddi.ayoub.mavenproject5;
 
-import java.awt.Button;
-import java.awt.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+// CORREGIDO: Usamos javafx, no java.awt
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-/**
- * FXML Controller class
- *
- * @author alumno
- */
 public class BotonesController implements Initializable {
-        @FXML
-        private Label label2;
-        @FXML
-        private Button bReset;
-        @FXML
-        private Button bOK;
-        @FXML
-        private Button bCancel;
-        @FXML
-        private Label label1;
-        @FXML
-        private Button boton1;
-        int contadorOK = 0;
-        int contadorCancel = 0;
-        
-    /**
-     * Initializes the controller class.
-     */
-        
-            public void pushButton(ActionEvent actionEvent) {
-             label1.setText("Ha pulsado el botón");
-             //compruebo qué objeto botón ha sido pulsado
-             //ok o cancel para contarlo
-             Object obj = actionEvent.getSource();
-             if (obj == bOK){
-             contadorOK ++;
-             label1.setText("Ha pulsado OK " + contadorOK + " veces");
-             }
-             if (obj == bCancel){
-             contadorCancel ++;
-             label1.setText("Ha pulsado Cancel " + contadorCancel + " veces");
-             }
-             if (obj == bReset){
-             label1.setText("Label1");
-             label2.setText("Tecla");
-             contadorOK = 0;
-             contadorCancel = 0;
-             }
-             }
-        
-            //codigos de las teclas
-//https://docs.oracle.com/javafx/2/api/javafx/scene/input/KeyCode.html
-            public void teclaPulsada(KeyEvent ke) {
-                KeyCode key = ke.getCode();
-             if (key == KeyCode.ENTER)
-             label2.setText("ENTER");
-             if (key == KeyCode.ESCAPE)
-             label2.setText("ESCAPE");
-            }
-            
+
+    // Los nombres aquí deben ser IDÉNTICOS a los fx:id del FXML
+    @FXML private Label label1;
+    @FXML private Label label2;
+    @FXML private Button reset;   // Antes bReset
+    @FXML private Button ok;      // Antes bOK
+    @FXML private Button cancel;  // Antes bCancel
+    @FXML private Button boton;   // Antes boton1 (para coincidir con tu FXML)
+
+    int contadorOK = 0;
+    int contadorCancel = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
+        // Inicialización
+    }
+
+    @FXML
+    public void pushButton(ActionEvent actionEvent) {
+        Object obj = actionEvent.getSource();
         
-    }    
-    
+        if (label1 != null) label1.setText("Ha pulsado un botón");
+
+        if (obj == ok) {
+            contadorOK++;
+            label1.setText("Ha pulsado OK " + contadorOK + " veces");
+        } else if (obj == cancel) {
+            contadorCancel++;
+            label1.setText("Ha pulsado Cancel " + contadorCancel + " veces");
+        } else if (obj == reset) {
+            label1.setText("Label1");
+            if(label2 != null) label2.setText("Tecla");
+            contadorOK = 0;
+            contadorCancel = 0;
+        } else if (obj == boton) {
+             label1.setText("Botón 1 presionado");
+        }
+    }
+
+    @FXML
+    public void teclaPulsada(KeyEvent ke) {
+        if(label2 == null) return;
+        
+        KeyCode key = ke.getCode();
+        if (key == KeyCode.ENTER) label2.setText("ENTER");
+        if (key == KeyCode.ESCAPE) label2.setText("ESCAPE");
+    }
 }
